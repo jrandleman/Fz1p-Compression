4ebf626118f2c6bd3ad7cf3bb36c5792784e094c
Jordan Randleman <jrandleman@scu.edu>
2019-06-04 21:58:49 -0700

    Enhanced newline & adjacent common word subs

--- ziptxt.c
+++ ziptxt.c
@@ -37,8 +37,8 @@ void write_txt_compressed(char *);
 void read_pass_ss_keys(char *);
 void write_ss_keys_to_single_str(char [][151], int, char *);
 /* COMMON WORD SUBSTITUTION FUNCTIONS */
-void splice_str(char *, char *, int, int);
-int delta_sub_common_words(char *, int, char [][50], char [][50]);
+void splice_str(char *, char *, int);
+int delta_sub_common_words(char *, char [][50], char [][50], int);
 void print_cw_used(int);
 /* SUB NUMBER OF SEQUENTIAL LETTERS & VISE-VERSA */
 void sequential_letters(char *);
@@ -81,70 +81,55 @@ void swap_str_sign(char [], unsigned char [], int, int);
 /* COMMON WORD SUBSTITUTIONS */
 #define FULL_CW_LEN 255
 char cw_keys[FULL_CW_LEN][50] = { /* roman numerals */
-	"_2_", "_3", "_4_", "_5_", "_6_", "_7", "_8", "_9_",
-	"_2\n", "_4\n", "_5\n", "_6\n", "_9\n",
+	"_2_", "_3", "_4_", "_5_", "_6_", "_7", "_8", "_9_", "_2\n", "_4\n", "_5\n", "_6\n", "_9\n",
 	/* numbers & punctuation */
 	"_jq", "_jw", "_jx", "_jy", "_jz", "_jt", "_js", "_jp", "_jn", "_jm",
-	"_jb", "jc_", "_jd_", "._?-", "!_?-", "?_?-", "_?-", "--", ".-", "!-", 
+	"_jb", "jc_", "_jd_", "._?-", "!_?-", "?_?-", "_?-", "--", "`-", "!-", 
 	/* single letter */
 	"_t_", "_o_", "_s_", "_w_", "_p_", "_d_", "_n_", "_r_", "_y_", "_f_", "_l_", "_h_", 
 	"_b_", "_g_", "_m_", "_e_", "_v_", "_u_", "_j_", "_x_", "_k_", "_z_", "_q_", 
 	/* two-letter 1 */
-	"_te_", "_ts_", "_tt_", "_td_", "_tn_", "_tr_", "_ty_", "_tf_", "_tl_", "_tg_", "_th_", 
-	"_ta_", "_tk_", "_tm_", "_tp_", "_tu_", "_tw_", "_tc_", "_ti_", "_ae_", "_ar_", "_ay_", 
-	"_af_", "_al_", "_ao_", "_ag_", "_ah_", "_aa_", "_ak_", "_ap_", "_au_", "_aw_", "_ac_", 
-	"_ai_", "_oe_", "_os_", "_ot_", "_od_", "_oy_", "_ol_", "_oo_", "_og_", "_ie_", "_id_", 
-	"_ir_", "_iy_", "_il_", "_io_", "_ig_", "_ih_", "_se_", "_ss_", "_st_", "_sd_", "_sn_", 
-	"_sr_", "_sy_", "_sf_", "_sl_", "_sg_", "_sh_", "_ws_", "_wt_", "_wd_", "_wn_", 
-	"_wr_", "_wy_",
+	"_te_", "_ts_", "_tt_", "_td_", "_tn_", "_tr_", "_ty_", "_tf_", "_tl_", "_tg_", "_th_", "_ta_", "_tk_", "_tm_", 
+	"_tp_", "_tu_", "_tw_", "_tc_", "_ti_", "_ae_", "_ar_", "_ay_", "_af_", "_al_", "_ao_", "_ag_", "_ah_", "_aa_", 
+	"_ak_", "_ap_", "_au_", "_aw_", "_ac_", "_ai_", "_oe_", "_os_", "_ot_", "_od_", "_oy_", "_ol_", "_oo_", "_og_", 
+	"_ie_", "_id_", "_ir_", "_iy_", "_il_", "_io_", "_ig_", "_ih_", "_se_", "_ss_", "_st_", "_sd_", "_sn_", 
+	"_sr_", "_sy_", "_sf_", "_sl_", "_sg_", "_sh_", "_ws_", "_wt_", "_wd_", "_wn_", "_wr_", "_wy_",
 	/* two-letter 2 */
-	"_wf_", "_wl_", "_wo_", "_wg_", "_wh_", "_ce_", "_cs_", "_ct_", "_cd_", "_cn_", 
-	"_cr_", "_cy_", "_cf_", "_cl_", "_co_", "_cg_", "_ch_", "_bs_", "_bt_", "_bd_", 
-	"_bb_", "_br_", "_bf_", "_bl_", "_bo_", "_bg_", "_pe_", "_ps_", "_pt_", "_pd_", 
-	"_pb_", "_pr_", "_py_", "_pf_", "_pl_", "_po_", "_pg_", "_hs_", "_ht_", 
-	"_hd_", "_hn_", "_hr_", "_hy_", "_hf_", "_hl_", "_ho_", "_hg_", "_fe_", "_fs_", 
-	"_ft_", "_fd_",
+	"_wf_", "_wl_", "_wo_", "_wg_", "_wh_", "_ce_", "_cs_", "_ct_", "_cd_", "_cn_", "_cr_", "_cy_", "_cf_", 
+	"_cl_", "_co_", "_cg_", "_ch_", "_bs_", "_bt_", "_bd_", "_bb_", "_br_", "_bf_", "_bl_", "_bo_", "_bg_", 
+	"_pe_", "_ps_", "_pt_", "_pd_", "_pb_", "_pr_", "_py_", "_pf_", "_pl_", "_po_", "_pg_", "_hs_", 
+	"_ht_", "_hd_", "_hn_", "_hr_", "_hy_", "_hf_", "_hl_", "_ho_", "_hg_", "_fe_", "_fs_", "_ft_", "_fd_",
 	/* no-space */
-	"`e", "!e", "?e", "`t", "!t", "?t", "`a", "!a", "?a", "`o", "!o", "?o", "`i", "!i", "?i", 
-	"`n", "!n", "?n", "`s", "!s", "?s", "`h", "!h", "?h", "`r", "!r", "?r", "`d", "!d", "?d", 
-	"`l", "!l", "?l", "`c", "!c", "?c", "`u", "!u", "?u", "`m", "!m", "?m", "`w", "!w", "?w", 
-	"`f", "!f", "?f", "`g", "!g", "?g", "`y", "!y", "?y", "`p", "!p", "?p", "`b", "!b", "?b", 
-	"`v", "?v", "!v", "`k", "!k", "?k", "`j", "?j", "!j", "`x", "?x", "!x", "`z", "`q", "?`",
-	"!?", "?!", "!`", "`?", "`!",
+	"`e", "!e", "?e", "`t", "!t", "?t", "`a", "!a", "?a", "`o", "!o", "?o", "`i", "!i", "?i", "`n", "!n", "?n", "`s", "!s", 
+	"?s", "`h", "!h", "?h", "`r", "!r", "?r", "`d", "!d", "?d", "`l", "!l", "?l", "`c", "!c", "?c", "`u", "!u", "?u", "`m", 
+	"!m", "?m", "`w", "!w", "?w", "`f", "!f", "?f", "`g", "!g", "?g", "`y", "!y", "?y", "`p", "!p", "?p", "`b", "!b", "?b", 
+	"`v", "?v", "!v", "`k", "!k", "?k", "`j", "?j", "!j", "`x", "?x", "!x", "`z", "`q", "?`","!?", "?!", "!`", "`?", "`!",
 };
 char cw_word[FULL_CW_LEN][50] = { /* roman numerals */
-	"_ii_", "_iii", "_iv_", "_v_", "_vi_", "_vii", "_iix", "_ix_",
-	"_ii\n", "_iv\n", "_v\n", "_vi\n", "_ix\n",
+	"_ii_", "_iii", "_iv_", "_v_", "_vi_", "_vii", "_iix", "_ix_", "_ii\n", "_iv\n", "_v\n", "_vi\n", "_ix\n",
 	/* numbers & punctuation */
-	"0", "1", "2", "3", "4", "5", "6", "7", "8", "9",
+	"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", 
 	"(", ")", "\t", ".\n", "!\n", "?\n", "\n", ":", ";", "/", 
 	/* single letter */
 	"_at_", "_as_", "_an_", "_be_", "_by_", "_do_", "_are_", "_in_", "_is_", "_it_", "_my_", "_of_", 
 	"_on_", "_or_", "_to_", "_and_", "_the_", "_have_", "_that_", "_this_", "_with_", "_you_", "_up_",
 	/* two-letter 1 */
-	"_any_", "_all_", "_but_", "_can_", "_day_", "_get_", "_him_", "_his_", "_her_", "_how_", "_now_", 
-	"_new_", "_out_", "_one_", "_our_", "_two_", "_use_", "_way_", "_who_", "_its_", "_say_", "_see_", 
-	"_she_", "_also_", "_come_", "_from_", "_for_", "_back_", "_give_", "_good_", "_just_", "_into_", "_know_", 
-	"_look_", "_like_", "_make_", "_most_", "_them_", "_over_", "_only_", "_some_", "_time_", "_take_", "_they_", 
-	"_work_", "_want_", "_will_", "_year_", "_your_", "_what_", "_when_", "_then_", "_than_", "_could_", "_well_", 
-	"_because_", "_even_", "_these_", "_which_", "_people_", "_about_", "_after_", "_first_", "_other_", "_think_", 
-	"_there_", "_their_", 
+	"_any_", "_all_", "_but_", "_can_", "_day_", "_get_", "_him_", "_his_", "_her_", "_how_", "_now_", "_new_", "_out_", "_one_", 
+	"_our_", "_two_", "_use_", "_way_", "_who_", "_its_", "_say_", "_see_", "_she_", "_also_", "_come_", "_from_", "_for_", "_back_", 
+	"_give_", "_good_", "_just_", "_into_", "_know_", "_look_", "_like_", "_make_", "_most_", "_them_", "_over_", "_only_", "_some_", "_time_", 
+	"_take_", "_they_", "_work_", "_want_", "_will_", "_year_", "_your_", "_what_", "_when_", "_then_", "_than_", "_could_", "_well_", 
+	"_because_", "_even_", "_these_", "_which_", "_people_", "_about_", "_after_", "_first_", "_other_", "_think_", "_there_", "_their_", 
 	/* two-letter 2 */
-	"_ask_", "_big_", "_bad_", "_eye_", "_few_", "_man_", "_own_", "_old_", "_try_", "_able_", 
-	"_case_", "_call_", "_fact_", "_find_", "_feel_", "_hand_", "_high_", "_life_", "_last_", "_long_", 
-	"_next_", "_part_", "_seem_", "_same_", "_tell_", "_week_", "_child_", "_company_", "_different_", "_early_", 
-	"_government_", "_group_", "_great_", "_important_", "_leave_", "_little_", "_large_", "_number_", "_not_", 
-	"_person_", "_place_", "_point_", "_problem_", "_public_", "_right_", "_small_", "_thing_", "_world_", "_woman_", 
-	"_young_", "_would_", 
+	"_ask_", "_big_", "_bad_", "_eye_", "_few_", "_man_", "_own_", "_old_", "_try_", "_able_", "_case_", "_call_", "_fact_", 
+	"_find_", "_feel_", "_hand_", "_high_", "_life_", "_last_", "_long_", "_next_", "_part_", "_seem_", "_same_", "_tell_", "_week_", 
+	"_child_", "_company_", "_different_", "_early_", "_government_", "_group_", "_great_", "_important_", "_leave_", "_little_", "_large_", "_number_", 
+	"_not_", "_person_", "_place_", "_point_", "_problem_", "_public_", "_right_", "_small_", "_thing_", "_world_", "_woman_", "_young_", "_would_", 
 	/* no-space */
-	"there", "tion", "then", "than", "well", "them", "some", "will", "that", "this", 
-	"with", "come", "back", "nter", "for", "ear", "different", "ent", "end", "see", 
-	"eye", "man", "use", "even", "able", "thing", "ing", "know", "now", "hand", 
-	"and", "make", "its", "not", "other", "the", "you", "any", "call", "ally", "all", 
-	"can", "most", "group", "over", "time", "take", "work", "place", "point", "right", 
-	"into", "person", "high", "last", "long", "part", "tell", "she", "ask", "big", 
-	"bad", "after", "ess", "but", "own", "ere", "try", "her", "how", "new", "out", 
-	"one", "our", "case", "way", "who", "ate", "get", "his",
+	"there", "tion", "then", "than", "well", "them", "some", "will", "that", "this", "with", "come", "back", "nter", "for", 
+	"ear", "different", "ent", "end", "see", "eye", "man", "use", "even", "able", "thing", "ing", "know", "now", "hand", "and", 
+	"make", "its", "not", "other", "the", "you", "any", "call", "ally", "all", "can", "most", "group", "over", "time", "take", 
+	"work", "place", "point", "right", "into", "person", "high", "last", "long", "part", "tell", "she", "ask", "big", "bad", "after", 
+	"ess", "but", "own", "ere", "try", "her", "how", "new", "out", "one", "our", "case", "way", "who", "ate", "get", "his",
 };
 /* GLOBAL VARIABLES */
 unsigned char CW_LEN = FULL_CW_LEN, cw_shift_up_idxs[FULL_CW_LEN]; /* store indices of rmvd cw_keys */
@@ -213,18 +198,15 @@ void show_txt_compressed(char *arg2, char *arg1) {
 	char s_buffer[150], filename[75];
 	int s_chunk_number = 0, i;
 	myAssert((fp = fopen(arg1, "r")), "\n\n(!!!) ERROR READING COMPRESSED TEXT FILE (!!!)\n");
-	while(fgets(s_buffer, 150, fp) > 0) { /* GET COMPRESSED TEXT */
-		char *p = s_buffer;
-		while(*(p + 1) != '\0') p++;
-		*p = '\0'; /* remove \n from s_buffer */
+	while(fgets(s_buffer, 150, fp) != NULL) { /* GET COMPRESSED TEXT */
+		s_buffer[strlen(s_buffer) - 1] = '\0'; /* remove \n from s_buffer */
 		if(zip_info == 1) printf("\nENCRYPTED/COMPRESSED: %s\n", s_buffer);
 		delta_txt_crypt(s_buffer, arg2); /* decrypt text */
 		sequential_numbers(s_buffer);
 		s_decompress(ss_array_matrix[s_chunk_number], s_buffer);
-		delta_sub_common_words(s_buffer, 151, cw_keys, cw_word); /* remove keys, insert words */
+		delta_sub_common_words(s_buffer, cw_keys, cw_word, 0); /* remove keys, insert words */
 		if(zip_info == 1) printf("DECRYPTED/DECOMPRESS: %s\n", s_buffer);
-		strcpy(s_compress_storage[s_chunk_number], s_buffer);
-		s_chunk_number++;
+		strcpy(s_compress_storage[s_chunk_number++], s_buffer);
 	}
 	for(i = 0; i < s_chunk_number; i++) {
 		char *p = s_max_buffer;
@@ -235,16 +217,13 @@ void show_txt_compressed(char *arg2, char *arg1) {
 	remove(arg1);
 	/* PUT DECOMPRESSED TEXT */
 	modify_str(s_max_buffer, 0); /* '_' => ' ' */
-	RMV_FILENAME_EXTENSION(arg1,strlen(".TZ1P.txt"));
-	ADD_FILENAME_EXTENSION(arg1,filename,".txt");
+	RMV_FILENAME_EXTENSION(arg1,strlen(".TZ1P.txt")); ADD_FILENAME_EXTENSION(arg1,filename,".txt");
 	myAssert((fp = fopen(filename, "w")), "\n\n(!!!) ERROR WRITING DECOMPRESSED TEXT FILE (!!!)\n");
 	fwrite(s_max_buffer, sizeof(char), strlen(s_max_buffer), fp);
 	fclose(fp);
-	return;
 }
 void read_txt_engl(char *arg2, char *arg1) {
-	char ss[45][151], s_chunk[151], temp_max_buff[MAX_CH];
-	int ret;
+	char ss[45][151], s_chunk[151], temp_max_buff[MAX_CH]; int ret;
 	FILE *fp;
 	myAssert((fp = fopen(arg1, "r")), "\n\n(!!!) ERROR READING TEXT FILE (CAPITAL STORING) (!!!)\n\n"); /* FILL CAPITALS/ADJUST CW */
 	fread(temp_max_buff, sizeof(char), MAX_CH, fp);
@@ -261,9 +240,7 @@ void read_txt_engl(char *arg2, char *arg1) {
 	fclose(fp);
 }
 void write_txt_compressed(char *arg1) {
-	FILE *fp;
-	int i;
-	char arg1seed[75], filename[75];
+	FILE *fp; int i; char arg1seed[75], filename[75];
 	strcpy(arg1seed, arg1);
 	RMV_FILENAME_EXTENSION(arg1seed,strlen(".txt"));
 	ADD_FILENAME_EXTENSION(arg1seed,filename,".TZ1P.txt");
@@ -292,9 +269,7 @@ void read_pass_ss_keys(char *arg2) {
 			ss_array_matrix[m][n+1][0] = '\0'; /* terminate ss 2d array */
 			m++; /* move to next 2d ss array (1 per s_chunk) in 3d matrix */
 			n = 0; /* start from row (ss) 0 */
-		} else {
-			n++; /* move to next row (ss) */
-		}
+		} else { n++; } /* move to next row (ss) */
 	}
 	fclose(fp);
 	remove(filename); /* delete password txt file once ss keys retrieved */
@@ -310,22 +285,20 @@ void write_ss_keys_to_single_str(char ss[][151], int ss_total, char *arg2) {
 /******************************************************************************
 * COMMON WORD SUBSTITUTION FUNCTIONS
 ******************************************************************************/
-void splice_str(char *s, char *sub, int splice_len, int size) {
-	char temp[size];
-	sprintf(temp, "%s%s", sub, s + splice_len);
-	strcpy(s, temp);
+void splice_str(char *s, char *sub, int splice_len) {
+	char temp[MAX_CH]; sprintf(temp, "%s%s", sub, s + splice_len); strcpy(s, temp);
 }
-int delta_sub_common_words(char *s, int total_len, char remove[][50], char insert[][50]) {
+int delta_sub_common_words(char *s, char remove[][50], char insert[][50], int hide_flag) {
 	int count = 0, found, word_len, i, j;
 	for(i = 0; i < CW_LEN; i++) {
 		char *p = s;
-		word_len = strlen(remove[i]);
-		found = 0;
+		word_len = strlen(remove[i]), found = 0;
+		if(hide_flag == 0 && strcmp(insert[i], "\n") == 0) sprintf(insert[i], "_\n");
 		while(*p != '\0') {
 			if(*p == remove[i][0]) {
 				for(j = 0; j < word_len; j++) if(*(p + j) != remove[i][j]) break;
-				if(j == word_len) { /* if word in s is common word */
-					splice_str(p, insert[i], word_len, total_len);
+				if(j == word_len) {
+					splice_str(p, insert[i], word_len);
 					if(zip_info == 1 && found == 0) { cw_idxs[count++] = i; found++; } /* store common words for 'info' command */
 				}
 			} else if(*p == '`' || *p == '!' || *p == '?') { p++; } /* avoid chaining words: themself => !uself => !?slf */
@@ -342,13 +315,12 @@ void print_cw_used(int sub_size) {
 * SUB NUMBER OF SEQUENTIAL LETTERS & VISE-VERSA
 ******************************************************************************/
 void sequential_letters(char *s) {
-	char *p = s, count_s[2];
-	int count;
+	char *p = s, count_s[2]; int count;
 	while(*(p + 1) != '\0') {
 		if(*(p + 1) == (*p + 1) && *(p + 2) == (*p + 2)) {
 			for(count = 3; (count < 7 && *(p + count) == (*p + count)); count++);
 			sprintf(count_s, "%d", --count);
-			splice_str(p + 1, count_s, count, 151);
+			splice_str(p + 1, count_s, count);
 		}
 		p++;
 	}
@@ -360,7 +332,7 @@ void sequential_numbers(char *s) {
 			int num = (int)(*p - 48), i;
 			for(i = 1; i <= num; i++) ch_arr[i - 1] = (*(p - 1) + i);
 			ch_arr[i - 1] = '\0';
-			splice_str(p, ch_arr, 1, 151);
+			splice_str(p, ch_arr, 1);
 		}
 		p++;
 	}
@@ -374,13 +346,11 @@ void delta_txt_crypt(char *char_array, char *arg2) {
 	srand((char)pass[0]);
 	while(*s != '\0') {
 		if(ch_count == key_size) {
-			ch_count = 0;
-			idx++;
+			ch_count = 0, idx++;
 			srand((char)pass[idx]);
 		}
 		*s ^= (rand()%11+5);
-		ch_count++;
-		s++;
+		ch_count++, s++;
 	}
 }
 /******************************************************************************
@@ -437,9 +407,8 @@ void modify_str(char *s, int sp_flag) {
 * CAPITAL WORD INDEX STORAGE
 ******************************************************************************/
 void delta_sub_capital_letters(char s[], int pack_flag) {
-	char *p = s + 1;
+	unsigned short word_idxs = 0, i = 0; char *p = s + 1;
 	caps.length = 0;
-	unsigned short word_idxs = 0, i = 0;
 	while(*(p + 1) != '\0') {
 		if(*p == ' ' || *p == '\n' || *p == '\t') { /* if is a word */
 			word_idxs++;
@@ -465,10 +434,7 @@ void hide_adjust_cw_keys(char s[]) {
 		while(*p != '\0') {
 			if(*p == modded_key[0]) {
 				for(j = 0; j < word_len; j++) if(*(p + j) != modded_key[j]) break;
-				if(j == word_len) {
-					shift_up_cw_keys(i);
-					cw_shift_up_idxs[FULL_CW_LEN - (CW_LEN--)] = (unsigned char)i;
-				}
+				if(j == word_len) { shift_up_cw_keys(i); cw_shift_up_idxs[FULL_CW_LEN - (CW_LEN--)] = (unsigned char)i; }
 			}
 			p++;
 		}
@@ -491,15 +457,14 @@ void s_decompress(char ss[][151], char *s) {
 	char *p = s, t[151];
 	while(*p != '\0') {
 		if(is_ss_ref(0, *p) == 1) {
-			sprintf(t, "%s%s", ss[ss_ch_index(*p)], p+1);
-			sprintf(p, "%s", t);
+			sprintf(t, "%s%s", ss[ss_ch_index(*p)], p+1); sprintf(p, "%s", t);
 			p = s;
 		} else { p++; }
 	}
 }
 int s_compress(char ss[][151], char *s) { /* returns # of substrings */
 	modify_str(s, 1); /* lowify & ' ' => '_' */
-	int sub_word_count = delta_sub_common_words(s, 151, cw_word, cw_keys); /* remove common words, insert keys */
+	int sub_word_count = delta_sub_common_words(s, cw_word, cw_keys, 1); /* remove common words, insert keys */
 	if(zip_info == 1) {
 		printf("SUBD WORDS => LEN: %lu, STR: %s\n", strlen(s), s);
 		print_cw_used(sub_word_count); /* print common words subbed and associated keys */
@@ -507,14 +472,12 @@ int s_compress(char ss[][151], char *s) { /* returns # of substrings */
 	int ss_idx = 0, found;
 	char *p = s, *r;
 	while(*(p + 2) != '\0') { /* first ss instance */
-		r = p + 2;
-		found = 0;
+		r = p + 2, found = 0;
 		while(*(r+1) != '\0') { if(*r == *p && *(r+1) == *(p+1)) { found++; rm_nxt_ch(r, ss_idx); } r++; } /* second ss instance */
 		if(found > 0) { /* if ss found, restart search for compounded ss */
 			store_ss(ss, p, ss_idx);
 			rm_nxt_ch(p, ss_idx);
-			ss_idx++;
-			p = s;
+			ss_idx++, p = s;
 		} else { p++; }
 	}
 	return clean_ss(ss, s, ss_idx);
@@ -557,8 +520,7 @@ char adjust_ref_char(char ss_ref_ch) {
 ******************************************************************************/
 void store_ss(char ss[][151], char *p, int ss_idx) { sprintf(ss[ss_idx], "%c%c", *p, *(p + 1)); }
 int clean_ss(char ss[][151], char *s, int ss_total) {
-	char *p;
-	int nest_idx = 0, nested_ss[45], i; /* nested ss only ref'd by other ss, arr holds their idxs */
+	char *p; int nest_idx = 0, nested_ss[45], i; /* nested ss only ref'd by other ss, arr holds their idxs */
 	for(i = 0; i < ss_total; i++) {
 		p = s;
 		while(*p != '\0') { if(*p == ss_refs[i]) { break; } p++; }
@@ -597,9 +559,8 @@ void trim_ss(char ss[][151], char *s, int nested_ss[45], int nest_total, int ss_
 		}
 		for (int k = i; k < nest_total; k++) nested_ss[k] = adjust_ref_char(nested_ss[k]); /* nested-ss idxs -= 1 ss_refs val per del */
 		ss[j][0] = '\0';
-		while(*p != '\0') { /* s ss references -= 1 ss_refs val as per del */
-			if(is_ss_ref((nested_ss[i]+ 1), *p) == 1) { *p = adjust_ref_char(*p); } p++;/* if *p = nested ref */
-		}
+		/* s ss references -= 1 ss_refs val as per del */
+		while(*p != '\0') { if(is_ss_ref((nested_ss[i]+ 1), *p) == 1) { *p = adjust_ref_char(*p); } p++; } /* if *p = nested ref */
 	}
 }
 void print_ss(char ss[][151], int ss_total) {
